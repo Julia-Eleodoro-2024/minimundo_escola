53cdc300a499312f7505e487de32ff9e99f9241c
julia-kw <148871757+julia-kw@users.noreply.github.com>
2024-03-26 11:18:17 -0300

    Professor.java

---
+++ src/main/java/Professor.java
@@ -0,0 +1,47 @@
+public class Profesor {
+    // Atributos
+    private String nome;
+    private int idade;
+    private String sexo;
+    
+    
+    // Construtor
+    public Professor(String nome, int idade, String sexo) {
+        this.nome = nome;
+      
+    }
+
+    // Métodos para acessar os atributos 
+    private String getNome() {
+        return this.nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+      }
+    
+     private void validarNome(String nome){
+       String regex
+       boolean teste = nome.matches(regex);
+       if(teste){
+         this.nome = nome
+     } else {
+         
+       }
+
+    private int getIdade() {
+        return this.idade;
+            }
+
+    private void int setIdade(int idade) {
+          this.idade = idade
+        }
+
+    private String getSexo() {
+        return this.sexo;
+    }
+
+   private void setSexo(String sexo) {
+       return.sexo = sexo
+       }
+ }
